8797d8652ca66341e9aebb0d6782c8d96aed352c
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-08 16:15:26 +0530

    Add initial screen for the website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,24 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 
 const IndexPage: React.FC<PageProps> = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+  return (
+    <div className="px-16 py-12">
+      <div className="flex float-right">
+        <div className="px-8">Home</div>
+        <div className="px-8">Projects</div>
+        <div className="px-8">Resume</div>
+        <div className="px-8">Contact</div>
+      </div>
+      <div className="my-24 mx-12">
+        <h3 className="text-8xl pt-4 font-bold">Hello,</h3>
+        <h1 className="text-8xl pt-4 font-bold">
+          I'm Yash Solanki<span>.</span>
+        </h1>
+        <h5 className="text-4xl font-extralight pt-4">Full Stack Developer</h5>
+      </div>
+    </div>
+  );
+}
 
 export default IndexPage;
 
