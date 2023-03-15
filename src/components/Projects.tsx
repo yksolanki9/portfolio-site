9cf6defc0509187e29c7ad78f91c51d1bb0a6c83
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-15 10:13:32 +0530

    Add fontawesome icons and create card for one project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,37 @@
 import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../utils/fontawesome";
+import { ProjectCard } from "./Project-card";
 
 export const Projects = () => {
-  return <div></div>;
+  return (
+    <div className="2xl:pt-64 mt-20 pt-28 px-16 h-screen bg-gray-800 about-section">
+      <h2 className="text-5xl text-center">Projects</h2>
+      <div className="grid grid-cols-2 justify-center justify-items-center">
+        <div className="card">
+          <div className="flex justify-between">
+            <div>Comet</div>
+            <div>
+              <FontAwesomeIcon icon={"arrow-up-right-from-square"} />
+            </div>
+          </div>
+
+          <div className="text-xs font-light pt-2">
+            A personal daily journal and self reflection mobile app.
+          </div>
+          <img src="https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white"></img>
+          <img src="https://img.shields.io/badge/Ionic-3880FF?style=for-the-badge&logo=ionic&logoColor=white"></img>
+          <img src="https://img.shields.io/badge/Capacitor-119EFF?style=for-the-badge&logo=Capacitor&logoColor=white"></img>
+          <FontAwesomeIcon icon={["fab", "angular"]} />
+          <FontAwesomeIcon icon={["fab", "google-play"]} />
+        </div>
+        <div className="card">
+          <p>Creating a card to see how it'll look in dark mode</p>
+        </div>
+        <div className="card">
+          <p>Creating a card to see how it'll look in dark mode</p>
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/utils/fontawesome.js
@@ -0,0 +1,8 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCoffee,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGooglePlay, faAngular } from "@fortawesome/free-brands-svg-icons";
+
+library.add([faCoffee, faArrowUpRightFromSquare, faGooglePlay, faAngular]);
