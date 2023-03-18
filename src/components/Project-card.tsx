5c28c394f79edada20f95f5f6554416646b0b0ae
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-18 22:25:10 +0530

    Fix badge position and description color

--- src/components/Project-card.tsx
+++ src/components/Project-card.tsx
@@ -8,19 +8,21 @@ export const ProjectCard = ({ project }) => {
   const { title, description, badges, redirections } = project;
   return (
     <a href={redirections.githubUrl} target={"_blank"}>
-      <div className="card">
+      <div className="card flex flex-col min-h-full">
         <div className="flex justify-between">
           <div>{title}</div>
           <div>
             <FontAwesomeIcon icon={"arrow-up-right-from-square"} />
           </div>
         </div>
-
-        <div className="text-xs font-light pt-2">{description}</div>
-
-        {badges.map((badge) => (
-          <img className="inline p-1 h-8" src={badgeUrlMapping[badge]} />
-        ))}
+        <div className="text-xs font-light pt-2 flex-1 text-slate-200">
+          {description}
+        </div>
+        <div className="pt-2 mt-auto">
+          {badges.map((badge) => (
+            <img className="inline pr-2 h-6" src={badgeUrlMapping[badge]} />
+          ))}
+        </div>
       </div>
     </a>
   );
