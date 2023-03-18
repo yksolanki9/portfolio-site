faaaeedd9cbdea6290d6401c67c4b85e5266ea5b
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-18 23:21:28 +0530

    Use map to loop over projects

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,33 @@
 import * as React from "react";
 
+const workExp = [
+  {
+    period: "Aug 2022 - Present",
+    title: "Fyle | Member of Technical Staff II",
+    description: "Angular, Ionic, Capacitor",
+  },
+  {
+    period: "Jan 2022 - Jul 2022",
+    title: "Fyle | Member of Technical Staff I",
+    description: "Angular, Ionic, Capacitor",
+  },
+  {
+    period: "Aug 2021 - Jan 2022",
+    title: "Fyle | Full Stack Developer Intern",
+    description: "Angular, Ionic, Capacitor",
+  },
+  {
+    period: "Jun 2021 - Jul 2021",
+    title: "Jeevam Health | Backend Intern",
+    description: "NodeJs, ExpressJs, MongoDB",
+  },
+  {
+    period: "May 2020 - Jul 2020",
+    title: "Samsung Research | SDE Intern",
+    description: "Matlab, NYUSIM, Channel Simulations",
+  },
+];
+
 export const Experience = () => {
   return (
     <div className="2xl:pt-64 pt-56 pb-40 px-16 h-fit bg-cyan-800 experience-section">
@@ -7,47 +35,19 @@ export const Experience = () => {
       <div className="page">
         <div className="section">
           <div className="top-container">
-            <div className="exp-container left">
-              <div className="date">Aug 2022 - Present </div>
-              <div className="content">
-                <div>Fyle | Member of Technical Staff II</div>
-                <div className="subtitle">Angular, Ionic, Capacitor</div>
-              </div>
-            </div>
-
-            <div className="exp-container right">
-              <div className="date">Jan 2022 - Jul 2022</div>
-              <div className="content">
-                <div>Fyle | Member of Technical Staff I</div>
-                <div className="subtitle">Angular, Ionic, Capacitor</div>
-              </div>
-            </div>
-
-            <div className="exp-container left">
-              <div className="date">Aug 2021 - Jan 2022</div>
-              <div className="content">
-                <div>Fyle | Full Stack Developer Intern</div>
-                <div className="subtitle">Angular, Ionic, Capacitor</div>
-              </div>
-            </div>
-
-            <div className="exp-container right">
-              <div className="date">Jun 2021 - Jul 2021</div>
-              <div className="content">
-                <div>Jeevam Health | Backend Intern</div>
-                <div className="subtitle">NodeJs, ExpressJs, MongoDB</div>
-              </div>
-            </div>
-
-            <div className="exp-container left">
-              <div className="date">May 2020 - Jul 2020</div>
-              <div className="content">
-                <div>Samsung Research | SDE Intern</div>
-                <div className="subtitle">
-                  Matlab, NYUSIM, Channel Simulations
+            {workExp.map((exp, index) => (
+              <div
+                className={
+                  "exp-container " + (index % 2 === 0 ? "left" : "right")
+                }
+              >
+                <div className="date">{exp.period}</div>
+                <div className="content">
+                  <div>{exp.title}</div>
+                  <div className="subtitle">{exp.description}</div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
