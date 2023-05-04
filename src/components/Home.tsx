70e17a145655a9cc124eb7f2f9ac629233d08f0a
yksolanki9 <yashsolanki1709@gmail.com>
2023-05-04 19:24:55 +0530

    Make home page responsive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,22 +2,24 @@ import * as React from "react";
 
 export const Home = (props: any) => {
   return (
-    <div className="pt-12 px-16 h-screen">
+    <div className="pt-12 lg:px-16 md:px-8 px-4 h-screen">
       <div className="flex float-right">
         <div className="px-6">Home</div>
         <div className="px-6">Experience</div>
         <div className="px-6">Projects</div>
-        <div className="px-6">Blogs</div>
+        {/* <div className="px-6">Blogs</div> */}
         <div className="px-6">Resume</div>
         <div className="px-6">Contact</div>
       </div>
-      <div className="my-24 mx-12">
-        <h3 className="text-8xl pt-4 font-bold">Hello,</h3>
-        <h1 className="text-8xl pt-4 font-bold">
+      <div className="my-24 lg:mx-12 mx-4">
+        <h3 className="sm:text-8xl text-6xl sm:pt-4 pt-24 font-bold">Hello,</h3>
+        <h1 className="sm:text-8xl text-6xl pt-4 font-bold">
           I'm {props.name}
           <span>.</span>
         </h1>
-        <h5 className="text-4xl font-extralight pt-4">{props.title}</h5>
+        <h5 className="sm:text-2xl text-xl font-extralight sm:pt-4 pt-10 lowercase">
+          {props.title}
+        </h5>
       </div>
     </div>
   );
