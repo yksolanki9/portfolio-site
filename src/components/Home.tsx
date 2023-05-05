2d95c31582f9f3feab288b7e426cb0d944555fa2
yksolanki9 <yashsolanki1709@gmail.com>
2023-05-05 22:55:11 +0530

    Make home page responsive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export const Home = (props: any) => {
   return (
-    <div className="pt-12 lg:px-16 md:px-8 px-4 h-screen">
+    <div className="pt-12 min-[1100px]:px-16 md:px-8 px-4 h-screen">
       <div className="flex float-right">
         <div className="px-6">
           <a href="#about">About</a>
@@ -21,13 +21,15 @@ export const Home = (props: any) => {
           <a href="#contact">Contact</a>
         </div>
       </div>
-      <div className="my-24 lg:mx-12 mx-4">
-        <h3 className="sm:text-8xl text-6xl sm:pt-4 pt-24 font-bold">Hello,</h3>
-        <h1 className="sm:text-8xl text-6xl pt-4 font-bold">
+      <div className="relative top-1/3 lg:mx-12 mx-4">
+        <h3 className="min-[1000px]:text-8xl min-[700px]:text-7xl min-[500px]:text-5xl text-3xl font-bold">
+          Hello,
+        </h3>
+        <h1 className="min-[1000px]:text-8xl min-[700px]:text-7xl min-[500px]:text-5xl text-3xl pt-4 font-bold">
           I'm {props.name}
           <span>.</span>
         </h1>
-        <h5 className="sm:text-2xl text-xl font-extralight sm:pt-4 pt-10 lowercase">
+        <h5 className="sm:text-2xl text-md font-extralight sm:pt-4 pt-10 lowercase">
           {props.title}
         </h5>
       </div>
