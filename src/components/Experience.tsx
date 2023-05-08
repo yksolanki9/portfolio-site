4169b5d2edd21cdf91f44b50bcb49c693a10eb6d
yksolanki9 <yashsolanki1709@gmail.com>
2023-05-08 23:06:07 +0530

    Fix header text size for smaller devices

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export const Experience = (props: any) => {
   return (
     <div className="2xl:pt-64 pt-56 pb-40 lg:px-16 px-2  h-fit bg-cyan-800 experience-section">
-      <h2 id="experience" className="text-5xl pt-4 text-center">
+      <h2 id="experience" className="text-3xl sm:text-5xl pt-4 text-center">
         Experience
       </h2>
       <div className="page">
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,8 +4,8 @@ import { ProjectCard } from "./Project-card";
 
 export const Projects = ({ projects }) => {
   return (
-    <div className="2xl:pt-64 mt-20 py-40 px-2 lg:px-16  bg-gray-800 project-section">
-      <h2 id="projects" className="text-5xl py-4 text-center">
+    <div className="2xl:pt-64 mt-20 py-40 px-2 lg:px-16 bg-gray-800 project-section">
+      <h2 id="projects" className="text-3xl sm:text-5xl py-4 text-center">
         Projects
       </h2>
       <div className="grid lg:grid-cols-2 w-100 md:w-9/12 lg:w-11/12 xl:w-9/12 grid-cols-1 gap-4 justify-center justify-items-center m-auto">
