58848688b8e124d668a9c5c0db02911a792018dc
yksolanki9 <yashsolanki1709@gmail.com>
2023-06-11 20:38:54 +0530

    Fix hover on touch screen devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
