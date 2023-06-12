2af55a80c61f041cbf7d58d3500a2a3052cfc0bd
yksolanki9 <yashsolanki1709@gmail.com>
2023-06-12 22:53:25 +0530

    Fix page title and add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,9 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Yash Solanki's Personal Website`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Yash Solanki's Portfolio`,
+    siteUrl: `https://www.thesolanki.netlify.app`,
+    description: `Yash Solanki's Personal website.`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -25,6 +26,12 @@ const config: GatsbyConfig = {
             file: `https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap`,
           },
         ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.png",
       },
     },
   ],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,4 +26,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Yash Solanki's Portfolio</title>;
